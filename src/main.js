b6ec8b7d9ec94282915dc4c3d8c86c806d6d85e9
Malte Wirz <mail@maltewirz.com>
2019-12-06 09:32:18 +0100

    global and local components

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.component('my-cmp', {
+let cmp = {
   data: () => {
    return { status: 'Critical'}
   },
   template: '<p>Server Status: {{ status }}</p>'
-});
+};
 
 new Vue({
   el: '#app',
+  components: {
+    'my-cmp':  cmp
+  }
+})
 
+new Vue({
+  el: '#app2',
+  components: {
+    'my-cmp':  cmp
+  }
 })
 
 // render: h => h(App),
