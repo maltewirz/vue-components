088b664152251bb3765a960e4c7050d124c461fd
Malte Wirz <mail@maltewirz.com>
2019-12-06 10:44:58 +0100

    merge exercise

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
-  <app-server-status></app-server-status>
+    <div class="container">
+        <Header> </Header>
+        <hr>
+        <div class="row">
+            <Servers></Servers>
+            <ServerDetails></ServerDetails>
+        </div>
+        <hr>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Servers from './components/Servers.vue';
+import ServerDetails from './components/ServerDetails.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+    components: {
+        Header,
+        Servers,
+        ServerDetails,
+        Footer
+    }
+}
+
 </script>
 
 <style>
+
 </style>
